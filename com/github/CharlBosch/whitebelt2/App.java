ea87f299c271141775de113dd33525bc86771577
Charl Bosch <charl.bosch@gmail.com>
2014-07-17 08:28:10 +0200

    Commit

---
+++ com/github/CharlBosch/whitebelt2/App.java
@@ -0,0 +1,18 @@
+package com.github.CharlBosch.whitebelt2;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class App {
+	
+	public static void main(String[] args) {
+		LocalDateTime now = LocalDateTime.now();
+		System.out.println("It is now :"+now);
+		
+		LocalDateTime tenMinutesFromNow = now.plus(10, ChronoUnit.MINUTES);
+		
+		System.out.println("now did not change, why?, it is Immutable (Will never change): " +now) ;
+		System.out.println("Ten minutes from now will be " +tenMinutesFromNow);
+		}
+	
+	}
